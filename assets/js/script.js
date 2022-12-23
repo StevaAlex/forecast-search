b737ed27f960d4f12cfe077c01758976c5ad3695
Steva <stevaalex@github.com>
2022-12-23 12:24:24 +0000

    Added API for geocode and weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
-$(document).ready(function () { 
-    
+$(document).ready(function () {
+    $("#search-button").on("click", function (event) {
+        var cityInput = ($("#search-input").val()).trim();//use .val to get value back from the input
+        var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1" + "&appid=96300bc0e7b9446d24a0aa1ec635d8ba";
+        // var queryURL = "api.openweathermap.org/data/2.5/forecast?lat=" +lat + "&lon=" lon + "&appid=" + "ENTER API KEY" 
+        var longitude;
+        var latitude;
+        var cityData;
+        event.preventDefault();
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(response);
+                cityData = response[0];
+                longitude = cityData.lon;
+                latitude = cityData.lat;
+
+                queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=96300bc0e7b9446d24a0aa1ec635d8ba";
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                })
+                    .then(function (response) {
+                        console.log("city data using long and lat " + response); 
+                        console.log(response);
+                    })
+            })
+    })
 })
