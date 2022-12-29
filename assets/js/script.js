c0161f66daee92355d78d27f764f329cbfe3bc18
Steva <stevaalex@github.com>
2022-12-29 19:17:42 +0000

    Added code for icon and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,27 +14,34 @@ $(document).ready(function () {
             method: "GET" //get call method
         })
             .then(function (response) {
-                console.log(response);
                 cityData = response[0];//the response comes back as an array, so this selects the first array item, which is an object
                 longitude = cityData.lon; //use the response to get back long and lat co-ordinates
                 latitude = cityData.lat;
-//add this second api within the then function so the long and lat co-ordinates can be fed into the weather api
+                //add this second api within the then function so the long and lat co-ordinates can be fed into the weather api
                 queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=96300bc0e7b9446d24a0aa1ec635d8ba";
                 $.ajax({
-                    url: queryURL, 
+                    url: queryURL,
                     method: "GET"
                 })
-                    .then(function (response) {  
+                    .then(function (response) {
+                        console.log(response);
                         //select section for todays weather 
                         var todaySection = $("#today");
                         //create header for today section
                         var todaysHeader = $("<h5>");
                         var currentDate = moment(response.list[0].dt_txt).format("D/M/YYYY"); //puts the current date in the format of D/M/YYYY
-                        var weatherIcon = response.list[0].weather[0].icon; //variable that selects icon from API
+                        var weatherIcon = response.list[0, 7, 0].weather[0].icon; //variable that selects icon from API is within an array matrix
+                        var weatherURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";//link for weather icon from open weather API
                         //assign text to header 
-                        todaysHeader.html(response.city.name +" " + currentDate); 
+                        todaysHeader.html(response.city.name + " " + "(" + currentDate +")");
                         //append to today section
                         todaySection.prepend(todaysHeader);
+                        //create img tag
+                        var imgTag = $("<img>");
+                        //assign source
+                        imgTag.attr("src", weatherURL);
+                        //append to today section
+                        todaysHeader.append(imgTag);//append to header so the icon appears directly after the text
                         // $("#card1"), $("#icon1").empty();
                         // console.log("city data using long and lat " + response); 
                         // console.log(response); 
